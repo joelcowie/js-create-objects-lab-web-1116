54b1c52af566d3a91645a69976dea6aec462c3bb
joelcowie <joel.a.cowie@gmail.com>
2017-01-20 13:15:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function Dog(name, breed, age) {
+  this.name = name
+  this.breed = breed
+  this.age = age
+}
+
+function Cat(name, breed, age) {
+  this.name = name
+  this.breed = breed
+  this.age = age
+}
+
+function Ferret(name, age, requirements) {
+  this.name = name
+  this.age = age
+  this.foodRequirements = requirements
+}
+
+function Bird(name, breed) {
+  this.name = name
+  this.breed = breed
+}
+
+function Fish(name, species, conditions) {
+  this.name = name
+  this.species = species
+  this.waterConditions = conditions
+}
